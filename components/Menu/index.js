939e48cf4f2a47587ffa7004d6612031003065a1
Quentin <quentin@adelios.fr>
2019-02-21 21:03:00 +0100

    moved global menu to bottom with demo numbers

--- components/Menu/index.js
+++ components/Menu/index.js
@@ -1,5 +1,30 @@
 import style from './style.scss';
+import Link from 'next/link';
+import {withRouter} from 'next/router';
 
-const Menu = props => <ul />;
+const pages = ['helmet'];
 
-export default Menu;
+const Menu = ({router}) => (
+	<nav className={style.menu}>
+		<p>Other demos :</p>
+		<ul>
+			{pages.map((page, i) => (
+				<li key={i}>
+					<Link href={page} prefetch>
+						<a
+							className={
+								router.pathname === '/' + page
+									? style.active
+									: null
+							}
+						>
+							{i + 1}
+						</a>
+					</Link>
+				</li>
+			))}
+		</ul>
+	</nav>
+);
+
+export default withRouter(Menu);
