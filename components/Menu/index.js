54966e61d58bf84487b3e6c08d5cda9febeb7768
Quentin <quentin@adelios.fr>
2019-02-21 19:39:33 +0100

    added _document and _app

---
+++ components/Menu/index.js
@@ -0,0 +1,5 @@
+import style from './style.scss';
+
+const Menu = props => <ul />;
+
+export default Menu;
---
+++ pages/_app.js
@@ -0,0 +1,74 @@
+import 'destyle.css';
+import '../theme/style.scss';
+import App, {Container} from 'next/app';
+import NextSeo from 'next-seo';
+import Menu from '../components/Menu';
+
+// https://github.com/garmeeh/next-seo#title-template
+
+const SEO = {
+	titleTemplate: `%s - ${process.env.title}`,
+	twitter: {
+		cardType: 'summary_large_image',
+		handle: '@Qu3ntinD',
+		// site: '@site',
+	},
+
+	openGraph: {
+		// url: '',
+		type: 'website',
+		locale: 'fr_FR',
+		// book: {
+		// 	authors: '(string[]) Writers of the article.',
+		// 	isbn: '(string) The ISBN',
+		// 	releaseDate: '(datetime) The date the book was released.',
+		// 	tags: '(string[]) Tag words associated with this book.',
+		// },
+		// article: {
+		// 	publishedTime: '(datetime) When the article was first published.',
+		// 	modifiedTime: '(datetime) When the article was last changed.',
+		// 	expirationTime: '(datetime) When the article is out of date after.',
+		// 	authors: '(string[]) Writers of the article.',
+		// 	section: '(string) A high-level section name. E.g. Technology',
+		// 	tags: '(string[]) Tag words associated with this article.',
+		// },
+	},
+};
+
+export default class MyApp extends App {
+	render() {
+		const {Component, pageProps} = this.props;
+		const {title, description} = pageProps;
+		const seoConfig = {
+			...SEO,
+			title,
+			description,
+			openGraph: {
+				title,
+				description,
+				// images: [
+				// 	{
+				// 		url: 'https://www.example.ie/og-image.jpg',
+				// 		width: 800,
+				// 		height: 600,
+				// 		alt: 'Og Image Alt',
+				// 	},
+				// 	{
+				// 		url: 'https://www.example.ie/og-image-2.jpg',
+				// 		width: 800,
+				// 		height: 600,
+				// 		alt: 'Og Image Alt 2',
+				// 	},
+				// ],
+			},
+		};
+
+		return (
+			<Container>
+				<NextSeo config={seoConfig} />
+				<Menu />
+				<Component {...pageProps} />
+			</Container>
+		);
+	}
+}
