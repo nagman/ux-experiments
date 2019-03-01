7a5e20954efa2eb34e3b8eca01156be2cb0bfc25
Quentin <quentin@adelios.fr>
2019-03-01 20:28:50 +0100

    created an Img component with srcset and sizes implemented

---
+++ components/Img/index.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+const Img = ({src, width, sizes, ...rest}) => {
+	const srcSets = [
+		`${src} ${width}w`,
+		`${src.replace('.', '@2x.')} ${width * 2}w`,
+	];
+
+	return (
+		<img
+			src={src}
+			srcSet={srcSets.join(', ')}
+			sizes={sizes || width + 'px'}
+			{...rest}
+		/>
+	);
+};
+
+Img.propTypes = {
+	src: PropTypes.string.isRequired,
+	alt: PropTypes.string.isRequired,
+	width: PropTypes.number.isRequired,
+	sizes: PropTypes.string,
+};
+
+export default Img;
